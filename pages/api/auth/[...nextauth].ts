28be2820e5b4345e072e2faa1bbbeebf7155f677
Arlindo <arlindojosboa@gmail.com>
2022-10-30 12:43:31 +0200

    FEATURE: Login By Magic Link

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,10 +1,24 @@
 import clientPromise from '@lib/mongodb'
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import NextAuth from 'next-auth'
+import EmailProvider from 'next-auth/providers/email'
+import FacebookProvider from 'next-auth/providers/facebook'
 import GoogleProvider from 'next-auth/providers/google'
 
 export const authOptions = {
   providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD
+        }
+      },
+      from: process.env.EMAIL_FROM
+    // maxAge: 24 * 60 * 60, // How long email links are valid for (default 24h)
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
@@ -12,6 +26,21 @@ export const authOptions = {
       profile (profile) {
         return {
           id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture
+        }
+      }
+    }),
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_ID!,
+      clientSecret: process.env.FACEBOOK_SECRET!,
+
+      profile (profile: any) {
+        console.log(profile)
+
+        return {
+          id: profile.id,
           name: profile.name,
           email: profile.email,
           image: profile.picture
